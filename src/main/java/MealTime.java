cb2b518886de01345f71e1a72eda92ee4c1ffb26
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-04 13:16:04 -0800

    Add BufferedReader class

--- src/main/java/MealTime.java
+++ src/main/java/MealTime.java
@@ -1,5 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MealTime {
     public static void main(String[] args) {
-        System.out.println("What did you eat for BreakFast")
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("What did you eat for breakfast?");
+            String yourBreakfast = bufferedReader.readLine();
+            System.out.println("You had " + yourBreakfast + " for breakfast.");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
